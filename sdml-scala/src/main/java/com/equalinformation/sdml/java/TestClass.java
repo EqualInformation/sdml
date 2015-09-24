02295ecbc949b94138b57166ee41837c261653e8
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-24 14:14:49 -0700

    SDML scala initial project files.

---
+++ sdml-scala/src/main/java/com/equalinformation/sdml/java/TestClass.java
@@ -0,0 +1,7 @@
+package com.equalinformation.sdml.java;
+
+/**
+ * Created by bpupadhyaya on 9/24/15.
+ */
+public class TestClass {
+}
---
+++ sdml-scala/src/test/scala/com/equalinformation/sdml/test/scala/TestScala.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.sdml.test.scala
+
+/**
+ * Created by bpupadhyaya on 9/24/15.
+ */
+object TestScala {
+
+}
